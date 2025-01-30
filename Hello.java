c2f4af710b9a3dd721286bb03dc79a1b9494ebb0
trelevenl1801 <trelevenl1801@my.uwstout.edu>
2025-01-29 20:49:18 -0600

    OriginalCode

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        String x = "Hello World";
+        String x = "Hello Universe";
         System.out.println(x);
     }
 }
